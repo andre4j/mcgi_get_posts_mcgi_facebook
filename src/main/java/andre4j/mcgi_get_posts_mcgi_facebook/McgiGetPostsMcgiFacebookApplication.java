a3633a96a3c9b3ef7ae341e6937ec806db33bb2c
André Valverde <devandrecontact@gmail.com>
2025-09-01 09:30:51 -0300

    initial commit

---
+++ src/main/java/andre4j/mcgi_get_posts_mcgi_facebook/McgiGetPostsMcgiFacebookApplication.java
@@ -0,0 +1,13 @@
+package andre4j.mcgi_get_posts_mcgi_facebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McgiGetPostsMcgiFacebookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McgiGetPostsMcgiFacebookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/andre4j/mcgi_get_posts_mcgi_facebook/McgiGetPostsMcgiFacebookApplicationTests.java
@@ -0,0 +1,13 @@
+package andre4j.mcgi_get_posts_mcgi_facebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McgiGetPostsMcgiFacebookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
